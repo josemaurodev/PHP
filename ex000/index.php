18e418b0b310ed36a0efb17c2c8e6328b336b26e
Jose Mauro <josemauro.dev@gmail.com>
2024-10-31 11:53:36 -0300

    php

---
+++ ex000/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ex000</title>
+</head>
+
+<body>
+  <h1>
+    <?php 
+      //Para começar o código em html, é só digitar ! e dar enter 
+      //Depois, onde quiser colocar o php, é só colocar php
+      //e dar enter \u{} para digitar códigos utf, emojipedia.org 
+      //lembrando que esse código \u{} nao funciona em php anteriores ao 7
+      print "Meu primeiro código em PHP \u{1F418}";
+    ?>
+  </h1>
+  <p>
+    Aqui é o paragrafo da nossa página
+  </p>
+</body>
+
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ex001</title>
+</head>
+
+<body>
+  <?php
+  //esse comando mostra as configurações do servidor
+    phpinfo();
+  ?>
+  <p>Aqui é nosso paragrafo</p>
+</body>
+
+</html>
