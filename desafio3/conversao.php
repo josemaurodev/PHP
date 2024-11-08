746121881c86e959a2b04190274ad3e5e0ff42f7
Jose Mauro <josemauro.dev@gmail.com>
2024-11-08 12:36:57 -0300

    using external api/correcting challenge 3

--- desafio3/conversao.php
+++ desafio3/conversao.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Conversor</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <header>
@@ -15,10 +15,10 @@
   $numberToConvert = $_GET["moeda"];
   $numberDolar = $numberToConvert*5.7;
 
-  $numberDolarFormat = number_format ($numberDolar, 2, '.',);
-  print ("Seus $numberToConvert equivalem a <strong>US$ $numberDolarFormat</strong>");
-  print ("</br>");
-  print ("<strong>Cotação do dólar é R$5,70</strong> informada diretamente no código");
+  $padraoInternacional = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+  print ("<p>Seus " . numfmt_format_currency($padraoInternacional, $numberToConvert, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padraoInternacional, $numberDolar, "USD")."</strong></p>");
+  print ("<p><strong>Cotação do dólar é R$5,70</strong> informada diretamente no código</p>");
 ?>
   <button onclick="window.location.href='index.html'">Voltar</button>
 
---
+++ desafio4/conversao.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conversor</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header></header>
+  <main>
+    <section>
+      <h1>Conversor de Moedas</h1>
+      <?php 
+
+      $inicio = date("m-d-Y", strtotime("-7 days"));
+      $fim = date("m-d-Y");
+      $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+      
+      $dados = json_decode(file_get_contents($url), true);
+      
+      $cotacao = $dados["value"][0]["cotacaoCompra"];
+      $real = $_GET["real"];
+      
+      $valorEmDol = $real * $cotacao;
+      $padraoInternacional = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+      
+      print ("<p>Seus " . numfmt_format_currency($padraoInternacional, $real, "BRL") . " equivalem a: <strong>" . numfmt_format_currency($padraoInternacional, $valorEmDol, "USD") ."</strong></p>");
+
+      print ("<p>Valor de cotação (" . numfmt_format_currency($padraoInternacional, $cotacao, "USD") . ") retirado diretamente do <a href='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/aplicacao#!/recursos/CotacaoDolarDia' target='_blank'>Banco Central</a></p>");
+      
+      ?>
+      <button onclick="window.location.href='index.html'">Voltar</button>
+    </section>
+  </main>
+</body>
+
+</html>
